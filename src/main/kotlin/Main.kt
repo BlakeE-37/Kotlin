c1fb37ab722fa3c085fa56f272f788e007d722e7
Blake Hollingsworth <Blake.Hollingsworth80@gmail.com>
2022-02-03 15:13:45 -0700

    first commit, added vendingmachine class and corresponding functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,54 @@
+class VendingMachine {
+    var snack = ""
+    var price = 0.0
+    var location = ""
+
+    //Print Snack name
+    fun printSnack(){
+        print(this.snack + "   ")
+    }
+
+    //Print Snack Price
+    fun printPrice(){
+        print(" $" + this.price + "     ")
+    }
+
+    //Print the location of the snack
+    fun printLocation(){
+        print("  " + this.location + "       ")
+    }
+}
+
 fun main() {
+    // Create VendingMachine objects
+val snack1 = VendingMachine()
+    snack1.snack = "Doritos"
+    snack1.price = 2.41
+    snack1.location = "A1"
 
+val snack2 = VendingMachine()
+    snack2.snack = "Skittles"
+    snack2.price = 1.75
+    snack2.location = "A2"
 
+val snack3 = VendingMachine()
+    snack3.snack = "Snickers"
+    snack3.price = 1.75
+    snack3.location = "A3"
+
+    //print the vending machine
+    //Print the object variables
+    println("\n-----------------------------")
+    snack1.printSnack()
+    snack2.printSnack()
+    snack3.printSnack()
+    print("\n")
+    snack1.printPrice()
+    snack2.printPrice()
+    snack3.printPrice()
+    print("\n")
+    snack1.printLocation()
+    snack2.printLocation()
+    snack3.printLocation()
+    println("\n-----------------------------")
 }
